b8bacd40fefc219bf23ebfbc07719c46465f1fff
Raadad Elsleiman <raadad@gmail.com>
2015-04-02 16:53:03 +1100

    if I spent 15 more minutes and had a working linter

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node Forecaste Tech Test
+
+#### Running:
+
+    source env_vars_local; node index.js
+
+
+#### Routes:
+
+    /weather/sydney - gives you weather of sydney today
+    /weather/japan/tomorrow - gives you weather of japan tomorrow
+    /weather/ - redirects to - /weather/sydney
--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-var http = require('http');
-var express = require('express');
-var app = module.exports = express();
-var debug = require('debug')('dev');
-var _ = require('underscore');  
+var http = require('http'),
+express = require('express'),
+app = module.exports = express(),
+debug = require('debug')('dev'),
+_ = require('underscore'),
+Forecast = require('forecast.io'),
+geocoder = require('geocoder');
 
-var Forecast = require('forecast.io');
-var geocoder = require('geocoder');
-
-if(! _.contains(['dev','test'],process.env.NODE_ENV)){
-	console.log('ERROR: Environment must be seto eg - dev, test');
-	process.exit(1);
+if (!_.contains(['dev', 'test'], process.env.NODE_ENV)) {
+    console.log('ERROR: Environment must be seto eg - dev, test');
+    process.exit(1);
 }
 
+require('./config')(app);
 
-require('./config')(app); 
-
-var forecast = new Forecast(app.get('config').forecast); 
-var weatherHandler =  new require('./lib/handlers/weatherHandler')(geocoder,forecast); 
-var weatherRoutes = require('./lib/routes/weatherRoutes')(weatherHandler); 
-var weatherRouter = require('./lib/routers/weatherRouter')(weatherRoutes);
+var forecast = new Forecast(app.get('config').forecast),
+weatherHandler = new require('./lib/handlers/weatherHandler')(geocoder, forecast),
+weatherRoutes = require('./lib/routes/weatherRoutes')(weatherHandler),
+weatherRouter = require('./lib/routers/weatherRouter')(weatherRoutes);
 
 app.set('views', __dirname + '/lib/views');
 app.set('view engine', 'jade');
 app.use('/weather', weatherRouter);
 
 http.createServer(app).listen(app.get('port'), function() {
-	debug('Application started on port %d', app.get('port'));
+    debug('Application started on port %d', app.get('port'));
 });
-
-
